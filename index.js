4343b6d398f55317d5eeb9005ec88cf79c2cbc47
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2021-08-27 11:30:09 +0800

    ip fix

--- index.js
+++ index.js
@@ -12,8 +12,10 @@ app.use(cors())
 const port = process.env.PORT || 3000
 
 app.get('/api/whoami', (req, res) => {
+  const ip = req.ip.startsWith('::ffff:') ? req.ip.replace('::ffff:', '') : req.ip
+
   res.json({
-    ipaddress: req.ip,
+    ipaddress: ip,
     language: req.headers['accept-language'],
     software: req.headers['user-agent'],
   })
